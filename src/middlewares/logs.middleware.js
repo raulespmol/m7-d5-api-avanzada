5d32c6229263be0f2c1a2ee852139d167b67fe7a
Raul Espinoza M <raul.espmol@gmail.com>
2024-06-02 21:33:06 -0400

    Update logs.middleware.js

--- src/middlewares/logs.middleware.js
+++ src/middlewares/logs.middleware.js
@@ -2,16 +2,19 @@ const moment = require('moment')
 
 const handleRequestLogs = (req, res, next) => {
   const time = moment().format('DD/MM/YY HH:mm:ss')
-  const {baseUrl, method, query, params} = req
-
+  const {method, query, params, originalUrl} = req
+  const path = originalUrl.split('?')[0]
+  
   const message = `
   *****
-  [${time}] Se ha realizado una consulta '${method} ${baseUrl}'
-  Queries: ${JSON.stringify(query, null, 2)}
-  Params: ${JSON.stringify(params, null, 2)}
+  [${time}] Se ha realizado una consulta '${method} ${path}'
+  Queries: 
+  ${JSON.stringify(query, null, 2)}
+  Params: 
+  ${JSON.stringify(params, null, 2)}
   *****
   `
-
+  
   console.log(message)
   next()
 }
